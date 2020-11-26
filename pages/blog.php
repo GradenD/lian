848b3c852b8e185cbd80e9bd9c3892e67740ed35
graden <bacharevia@mail.ru>
2020-11-26 17:10:31 +0500

    blog 55%

--- pages/blog.php
+++ pages/blog.php
@@ -10,127 +10,148 @@
 <div class="container">
     <div class="blog">
         <div class="blog-content">
-            <div class="blog-items">
-                <div class="blog-body">
-                    <a href="#">
-                        <div class="head-items">
-                            <h2>WORDPRESS</h2>
-                            <h1>I am a Blog Post Title</h1>
-                            <p class="name"><i class="fas fa-user"></i> by Jenn Pereira</p>
-                        </div>
-                        <div class="text">
-                            <p>
-                                Phasellus et nisl tellus. Etiam facilisis eu nisi 
-                                scelerisque faucibus. Proin semper suscipit magna, 
-                                nec imperdiet lacus semper vitae. Sed hendrerit enim non 
-                                justo posuere placerat eget purus mauris. Etiam facilisis 
-                                eu nisi scelerisque faucibus...
-                            </p>
-                        </div>
-                    </a>
-                    <div class="foot-items">
-                        <div class="date">
-                            <p><i class="far fa-calendar"></i>  on July 19, 2016</p>
-                        </div>
-                        <div class="social">
-                            <p>
-                                <i class="fas fa-share-alt" aria-hidden="true"></i>
-                                <i class="fas fa-heart"></i>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="blog-items">
-                <img src="img/index-block/index-10.jpg">
-                <div class="blog-body">
-                    <a href="#">
-                        <div class="head-items">
-                            <h1>I am a Blog Post with an Awesome Image</h1>
-                            <p class="name"><i class="fas fa-user"></i> by Jenn Pereira</p>
-                        </div>
-                        <div class="text">
-                            <p>
-                                Proin semper suscipit magna, nec imperdiet lacus 
-                                semper vitae. Sed hendrerit enim non justo posuere 
-                                placerat eget purus mauris...
-                            </p>
-                        </div>
-                    </a>
-                    <div class="foot-items">
-                        <div class="date">
-                            <p><i class="far fa-calendar"></i>  on July 19, 2016</p>
-                        </div>
-                        <div class="social">
-                            <p>
-                                <i class="fas fa-share-alt" aria-hidden="true"></i>
-                                <i class="fas fa-heart"></i>
-                            </p>
+            <div class="blog-columns">
+                <div class="blog-items">
+                    <div class="blog-body">
+                        <a href="#">
+                            <div class="head-items">
+                                <h2>WORDPRESS</h2>
+                                <h1>I am a Blog Post Title</h1>
+                                <p class="name"><i class="fas fa-user"></i> by Jenn Pereira</p>
+                            </div>
+                            <div class="text">
+                                <p>
+                                    Phasellus et nisl tellus. Etiam facilisis eu nisi 
+                                    scelerisque faucibus. Proin semper suscipit magna, 
+                                    nec imperdiet lacus semper vitae. Sed hendrerit enim non 
+                                    justo posuere placerat eget purus mauris. Etiam facilisis 
+                                    eu nisi scelerisque faucibus...
+                                </p>
+                            </div>
+                        </a>
+                        <div class="foot-items">
+                            <div class="date">
+                                <p><i class="far fa-calendar"></i>  on July 19, 2016</p>
+                            </div>
+                            <div class="social">
+                                <p>
+                                    <i class="fas fa-share-alt" aria-hidden="true"></i>
+                                    <i class="fas fa-heart"></i>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="blog-items">
-                <img src="img/index-block/index-9.jpg">
-                <div class="blog-body">
-                    <a href="#">
-                        <div class="head-items">
-                            <h1>I am a Blog Post with an Awesome Image</h1>
-                            <p class="name"><i class="fas fa-user"></i> by Jenn Pereira</p>
-                        </div>
-                        <div class="text">
-                            <p>
-                                Proin semper suscipit magna, nec imperdiet lacus 
-                                semper vitae. Sed hendrerit enim non justo posuere 
-                                placerat eget purus mauris...
-                            </p>
-                        </div>
-                    </a>
-                    <div class="foot-items">
-                        <div class="date">
-                            <p><i class="far fa-calendar"></i>  on July 19, 2016</p>
-                        </div>
-                        <div class="social">
-                            <p>
-                                <i class="fas fa-share-alt" aria-hidden="true"></i>
-                                <i class="fas fa-heart"></i>
-                            </p>
+                <div class="blog-items">
+                    <img src="img/index-block/index-10.jpg">
+                    <div class="blog-body">
+                        <a href="#">
+                            <div class="head-items">
+                                <h1>I am a Blog Post with an Awesome Image</h1>
+                                <p class="name"><i class="fas fa-user"></i> by Jenn Pereira</p>
+                            </div>
+                            <div class="text">
+                                <p>
+                                    Proin semper suscipit magna, nec imperdiet lacus 
+                                    semper vitae. Sed hendrerit enim non justo posuere 
+                                    placerat eget purus mauris...
+                                </p>
+                            </div>
+                        </a>
+                        <div class="foot-items">
+                            <div class="date">
+                                <p><i class="far fa-calendar"></i>  on July 19, 2016</p>
+                            </div>
+                            <div class="social">
+                                <p>
+                                    <i class="fas fa-share-alt" aria-hidden="true"></i>
+                                    <i class="fas fa-heart"></i>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="blog-items">
-                <div class="blog-body">
-                    <a href="#">
-                        <div class="head-items">
-                            <h2>WORDPRESS</h2>
-                            <h1>I am a Blog Post Title</h1>
-                            <p class="name"><i class="fas fa-user"></i> by Jenn Pereira</p>
-                        </div>
-                        <div class="text">
-                            <p>
-                                Phasellus et nisl tellus. Etiam facilisis eu nisi 
-                                scelerisque faucibus. Proin semper suscipit magna, 
-                                nec imperdiet lacus semper vitae. Sed hendrerit enim non 
-                                justo posuere placerat eget purus mauris. Etiam facilisis 
-                                eu nisi scelerisque faucibus...
-                            </p>
-                        </div>
-                    </a>
-                    <div class="foot-items">
-                        <div class="date">
-                            <p><i class="far fa-calendar"></i>  on July 19, 2016</p>
-                        </div>
-                        <div class="social">
-                            <p>
-                                <i class="fas fa-share-alt" aria-hidden="true"></i>
-                                <i class="fas fa-heart"></i>
-                            </p>
+                <div class="blog-items">
+                    <img src="img/index-block/index-9.jpg">
+                    <div class="blog-body">
+                        <a href="#">
+                            <div class="head-items">
+                                <h1>I am a Blog Post with an Awesome Image</h1>
+                                <p class="name"><i class="fas fa-user"></i> by Jenn Pereira</p>
+                            </div>
+                            <div class="text">
+                                <p>
+                                    Proin semper suscipit magna, nec imperdiet lacus 
+                                    semper vitae. Sed hendrerit enim non justo posuere 
+                                    placerat eget purus mauris...
+                                </p>
+                            </div>
+                        </a>
+                        <div class="foot-items">
+                            <div class="date">
+                                <p><i class="far fa-calendar"></i>  on July 19, 2016</p>
+                            </div>
+                            <div class="social">
+                                <p>
+                                    <i class="fas fa-share-alt" aria-hidden="true"></i>
+                                    <i class="fas fa-heart"></i>
+                                </p>
+                            </div>
                         </div>
                     </div>
+                </div>
+                <div class="blog-items">
+                    <div class="blog-body">
+                        <a href="#">
+                            <div class="head-items">
+                                <h2>WORDPRESS</h2>
+                                <h1>I am a Blog Post Title</h1>
+                                <p class="name"><i class="fas fa-user"></i> by Jenn Pereira</p>
+                            </div>
+                            <div class="text">
+                                <p>
+                                    Phasellus et nisl tellus. Etiam facilisis eu nisi 
+                                    scelerisque faucibus. Proin semper suscipit magna, 
+                                    nec imperdiet lacus semper vitae. Sed hendrerit enim non 
+                                    justo posuere placerat eget purus mauris. Etiam facilisis 
+                                    eu nisi scelerisque faucibus...
+                                </p>
+                            </div>
+                        </a>
+                        <div class="foot-items">
+                            <div class="date">
+                                <p><i class="far fa-calendar"></i>  on July 19, 2016</p>
+                            </div>
+                            <div class="social">
+                                <p>
+                                    <i class="fas fa-share-alt" aria-hidden="true"></i>
+                                    <i class="fas fa-heart"></i>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+            <div class="row">
+                <div class="col-12 col-pad load-more">
+                    <button class="btn-send"> 
+				         LOAD MORE
+			        </button>
                 </div>
             </div>
         </div>
-        <div class="blog-social"></div>
+        <div class="blog-social">
+            <div class="input-group md-form form-sm form-2 pl-0 blog-search">
+                <input class="form-control my-0 py-1 amber-border" type="text" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                    <span class="input-group-text amber lighten-3"><i class="fas fa-search text-grey" aria-hidden="true"></i></span>
+                 </div>
+            </div>
+            <div class="recent-posts">
+                <div class="post-head">
+                    <h1>RECENT POST</h1>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
